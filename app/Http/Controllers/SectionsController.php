39bff827700fc933d7f6669a6225625bbc58463d
engmohamedkh <engmohamedkh21@gmail.com>
2021-12-14 17:10:00 +0200

    this my new edit

---
+++ app/Http/Controllers/SectionsController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\sections;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SectionsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        $sec=sections::all();
+        return view('sections.sections',compact('sec'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        
+        $validated = $request->validate([
+            'section_name' => 'required|unique:sections|max:255',
+            'description' => 'required',
+        ],[
+
+            'section_name.required' =>'يرجي ادخال اسم القسم',
+            'section_name.unique' =>'اسم القسم مسجل مسبقا',
+            'description.required' =>'يرجي ادخال البيان',
+
+        ]);
+
+        sections::create([
+            'section_name' => $request->section_name,
+            'description' => $request->description,
+
+            'Created_by' => (Auth::user()->name),
+
+        ]);
+        session()->flash('edit','تم تعديل القسم بنجاج');
+         return redirect('/sections');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\sections  $sections
+     * @return \Illuminate\Http\Response
+     */
+    public function show(sections $sections)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\sections  $sections
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(sections $sections)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\sections  $sections
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, sections $sections)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\sections  $sections
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(sections $sections)
+    {
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\InvoicesController;
+use App\Http\Controllers\SectionsController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::resource('invoices', InvoicesController::class);
+Route::resource('sections', SectionsController::class);
+Route::get('/{page}', [AdminController::class,'index']);
+
+
